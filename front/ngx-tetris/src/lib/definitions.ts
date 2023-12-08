ef52a5633141d69fd79df763167f5f05089fd69a
Tasnim <tasnimmtir586@gmail.com>
2023-12-08 02:51:45 +0100

    !

--- front/ngx-tetris/src/lib/definitions.ts
+++
@@ -1,6 +0,0 @@
-export type PieceMap = Array<Array<number|undefined>>;
-export type PieceMaps = Array<PieceMap>;
-export interface GridSize {
-  width: number;
-  height: number;
-}
--- front/ngx-tetris/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-tetris
- */
-
-export * from './lib/tetris-core.module';
-export * from './lib/tetris-core.component';
